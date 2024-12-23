151240ae0d840cc834307d9e40c216bbea6b0984
Jay panchal <jaypanchal06334@gmail.com>
2024-12-23 14:24:11 +0530

    some changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -20,7 +20,7 @@ function Home() {
           <div className="flex flex-wrap">
             <div className="p-2 w-full">
               <h1 className="text-2xl font-bold hover:text-gray-500">
-                Loading.....
+                Blog are show after Login
               </h1>
             </div>
           </div>
